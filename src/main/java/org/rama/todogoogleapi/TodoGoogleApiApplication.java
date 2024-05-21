127111a77511ae0eab09669268301c9530839b92
Rama <sadamramanayudu@gmail.com>
2024-05-21 14:37:33 -0500

    Google API configuration added

--- src/main/java/org/rama/todogoogleapi/TodoGoogleApiApplication.java
+++ src/main/java/org/rama/todogoogleapi/TodoGoogleApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoGoogleApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoGoogleApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoGoogleApiApplication.class, args);
+    }
 
 }
