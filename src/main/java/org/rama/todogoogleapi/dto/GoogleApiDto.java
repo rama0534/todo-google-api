6af96099e72701f1dba9c54282545ac297e51b12
Rama <sadamramanayudu@gmail.com>
2024-05-21 14:40:55 -0500

    Model POJO's

---
+++ src/main/java/org/rama/todogoogleapi/dto/GoogleApiDto.java
@@ -0,0 +1,13 @@
+package org.rama.todogoogleapi.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class GoogleApiDto {
+    private String spreadSheetId;
+    private String range;
+    private String sheetName;
+    private List<List<Object>> dataToBeUpload;
+}
